c9393391c1e1f6207d4596e3f3b050cd40c04bf5
Mark Branscum <mbstudios@hotmail.com>
2020-08-19 18:15:46 -0500

    Alert Bar done ... styles complete responsiveness in effect All good

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-const greeting = "Welcome to my WebApp DashBoard";
+const alertBar = document.querySelector(".alert-bar");
+const closeBtn = document.querySelector(".close-alert");
+
+closeBtn.addEventListener("click", () => {
+   alertBar.style.display = "none";
+});
