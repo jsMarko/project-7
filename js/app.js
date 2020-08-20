32f9e50086ac4eefcc75246b44111c51bf50f5d5
Mark Branscum <mbstudios@hotmail.com>
2020-08-19 23:12:39 -0500

    Adding my first chart but its not showing up

--- js/app.js
+++ js/app.js
@@ -4,3 +4,31 @@ const closeBtn = document.querySelector(".close-alert");
 closeBtn.addEventListener("click", () => {
    alertBar.style.display = "none";
 });
+
+let trafficChart = document.querySelector("#chartjs-traffic").getContext("2d");
+
+let trafficLine = new chart("chartjs-traffic", {
+   type: "line",
+   data: {
+      labels: [
+         "16-22",
+         "23-29",
+         "30-5",
+         "6-12",
+         "13-19",
+         "20-26",
+         "17-3",
+         "4-10",
+         "11-17",
+         "18-24",
+         "25-31"
+      ],
+      datasets: [
+         {
+            label: "TRAFFIC",
+            data: [500, 1000, 1500, 2000, 2500]
+         }
+      ]
+   },
+   options: {}
+});
